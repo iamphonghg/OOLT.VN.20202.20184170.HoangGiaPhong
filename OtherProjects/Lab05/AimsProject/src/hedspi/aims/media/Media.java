9c713743a9617f573d05b592575d6b9db9c5a48c
Phong Hoang Gia <gioxoay.xp@gmail.com>
2021-05-30 12:49:27 +0700

    refactor lab05

---
+++ OtherProjects/Lab05/AimsProject/src/hedspi/aims/media/Media.java
@@ -0,0 +1,20 @@
+package hedspi.aims.media;
+
+public class Media {
+    private String title;
+    private String category;
+    private float cost;
+
+    public Media(String title) {
+        this.title = title;
+    }
+
+    public Media(String title, String category) {
+        this(title);
+        this.category = category;
+    }
+
+    public Media() {
+
+    }
+}
---
+++ OtherProjects/Lab05/OtherProjects/src/GarbageCreator.java
@@ -0,0 +1,14 @@
+public class GarbageCreator {
+    public String s;
+    public GarbageCreator() {
+        super();
+        this.s = "";
+    }
+    public void createGarbage() {
+        for (long i=0; i < 999999999; i++) {
+            s+=i*999999999;
+
+        }
+        System.out.println(s);
+    }
+}
